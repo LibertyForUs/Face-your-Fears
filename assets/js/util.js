1dd259e331fa43ee225be192e5c93045c1cf62eb
Qasim Qureshi <qasim.qureshi@gmail.com>
2020-08-15 11:24:51 +0500

    Updates to Oc carrying objects across the Z-index. Positioning now accounts for scaling

--- assets/js/util.js
+++ assets/js/util.js
@@ -18,3 +18,22 @@ function getWidth() {
       document.documentElement.scrollHeight, 
       document.documentElement.offsetHeight );
 	}
+
+	document.addEventListener('DOMContentLoaded', (event) => {
+		
+		if( window.location.search.indexOf('testing') !== -1 ){
+			let button = document.createElement('div');
+			button.classList.add('testBtn');
+			button.style.height = 100;
+			button.style.width = 100;
+			button.style.position = 'fixed';
+			button.style.left = 1400;
+			button.style.bottom = 200;
+			button.style.backgroundColor = 'blue';
+			button.style.zIndex = 9999;
+			
+			button.style.width = button.style.height = 100;
+
+			document.querySelector('#above-ground').appendChild(button);
+		}
+	})
