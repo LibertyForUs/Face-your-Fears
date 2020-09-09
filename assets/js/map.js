176e6cb2cdcedff1d26473a67f98b92a1dbcdc9d
Qasim Qureshi <qasim.qureshi@gmail.com>
2020-09-09 15:23:01 +0500

    Added mailbox

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,7 @@
 // Loading in map files & parsing items
 
+
+
 // Items to be rendered on-screen. Fixed items (like the fence) don't get parrallax
 let items = [
     {
@@ -8,5 +10,19 @@ let items = [
         fixed: false,
         position: {x: 2000, z: 4},
         isHeld: false
+    },
+    {
+        name: 'mailbox',
+        item: document.querySelector('#mailbox'),
+        fixed: true,
+        position: {x: 1200, z: 5},
+        isHeld: false
     }
 ]
+
+let mailboxData = items.find(item => item.name === "mailbox"),
+    mailbox = mailboxData.item;
+
+mailbox.style.left = mailboxData.position.x;
+mailbox.setAttribute('z', mailboxData.position.z);
+setPosition(mailbox);
