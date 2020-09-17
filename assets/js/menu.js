9164615ec8d2f16a091ed63ac4b69b3fb6e19cb4
Qasim Qureshi <qasim.qureshi@gmail.com>
2020-09-17 11:12:44 +0500

    Cross-browser audio playback experimentation with promises

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,16 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+
+    // Play menu audio
+    // document.querySelector('audio').play();
+    let audio = new Audio('/audio/title-song.mp3');
     
+    document.addEventListener('mousemove', (event) => {
+        let playPromise = audio.play();
+        document.removeEventListener('mousemove', this);    
+    })
+
+
+    // Tutorial button, updates menu state to show tutorial confirmation
     document.querySelector('.tutorial').addEventListener('click', (event) => {
         document.querySelector('.top .confirmation').classList.remove('dn');
         document.querySelector('.bottom.confirmation').classList.remove('dn');
@@ -7,6 +18,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         document.querySelector('.bottom.menu').classList.add('dn');
     })
 
+    // Tutorial cancel button, returns menu UI state to main-menu
     document.querySelector('.no').addEventListener('click', (event) => {
         document.querySelector('.top .confirmation').classList.add('dn');
         document.querySelector('.bottom.confirmation').classList.add('dn');
